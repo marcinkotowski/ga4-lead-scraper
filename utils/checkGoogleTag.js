e3e0cc01abee4560e152a87e45dedcd2809cbf41
marcinkotowski <marcin.kotowski02@gmail.com>
2023-03-23 00:20:21 +0100

    Add error handling in checkGoogleTag

--- utils/checkGoogleTag.js
+++ utils/checkGoogleTag.js
@@ -1,35 +1,28 @@
 import { TimeoutError } from "puppeteer-core";
 
 export async function checkGA4(website, browser) {
-  const page = await browser.newPage();
-
   try {
-    // Website with https also works
-    await page.goto(`http://${website}`, { waitUntil: "domcontentloaded" });
+    const page = await browser.newPage();
+
+    await page.goto(`http://${website}`, {
+      waitUntil: "domcontentloaded",
+      timeout: 10000,
+    });
+
+    const hasGA4 = await page.evaluate(() => {
+      const scripts = Array.from(document.querySelectorAll("script[src]"));
+      return scripts.some(
+        (script) =>
+          script.src.includes("gtag/js") && script.src.includes("id=G")
+      );
+    });
+
+    await page.close();
+
+    return hasGA4;
   } catch (err) {
-    if (err instanceof TimeoutError) {
-      try {
-        // Retry page loading with longer timeout
-        await page
-          .goto(`http://${website}`, {
-            waitUntil: "domcontentloaded",
-            timeout: 10000,
-          })
-          .catch(() => {
-            console.log(`${website} takes too long to load`);
-          });
-      } catch (err) {}
-    }
+    console.log(`${website} generate error: ${err}`);
+    // True means that it's not a lead
+    return true;
   }
-
-  const hasGA4 = await page.evaluate(() => {
-    const scripts = Array.from(document.querySelectorAll("script[src]"));
-    return scripts.some(
-      (script) => script.src.includes("gtag/js") && script.src.includes("id=G")
-    );
-  });
-
-  await page.close();
-
-  return hasGA4;
 }
