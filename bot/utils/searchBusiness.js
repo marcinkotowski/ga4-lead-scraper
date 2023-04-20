eda3244eea282602cf4053c5874e44be43a8a8d4
marcinkotowski <marcin.kotowski02@gmail.com>
2023-04-20 16:28:39 +0200

    Fix searching bug

--- bot/utils/searchBusiness.js
+++ bot/utils/searchBusiness.js
@@ -2,21 +2,17 @@ import puppeteer from "puppeteer-core";
 
 export async function searchBusiness(arg, page) {
   try {
-    const searchInput = await page.$("input#searchboxinput");
-
-    await searchInput.evaluate((input) => {
-      input.value = "";
-    }, searchInput);
-
-    await searchInput.type(arg);
+    await page.evaluate((searchKey) => {
+      const searchInput = document.querySelector("input#searchboxinput");
+      searchInput.value = searchKey;
+      searchInput.focus();
+    }, arg);
 
     const searchButton = await page.$("button#searchbox-searchbutton");
 
     await searchButton.click();
 
-    await page.waitForNavigation({
-      waitUntil: "networkidle0",
-    });
+    await page.waitForNetworkIdle();
   } catch (err) {
     throw new Error(`Error in searchBusiness function: ${err}`);
   }
